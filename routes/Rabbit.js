4edb8f251de6f829a175bf95358561d2718290c2
Sravani Gangula <s545682@nwmissouri.edu>
2021-11-18 23:56:51 -0600

    changes for ass13 and passport are updated

--- routes/Rabbit.js
+++ routes/Rabbit.js
@@ -3,6 +3,6 @@ const Rabbit_controlers= require('../controllers/Rabbit');
 var router = express.Router();
 
 // GET request for one Rabbit. 
-router.get('/Rabbits/:id', Rabbit_controlers.Rabbit_detail); 
+router.get('/', Rabbit_controlers.Rabbit_view_all_Page); 
 
 module.exports = router;
